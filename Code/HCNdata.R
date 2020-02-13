a1d04b04af22a3a55677f671102a718c2b99ebd0
jdpeiffer6 <jdp6n8@mail.missouri.edu>
2020-02-12 22:56:44 -0600

    Made some changes

---
+++ Code/HCNdata.R
@@ -0,0 +1,9 @@
+setwd("C:\\Users\\JD\\Documents\\StateParks\\Data")
+
+stations <- ghcnd_stations()
+MOstations=subset(stations,state=='MO')
+
+over40=subset(MOstations,first_year<=1981)
+uptonow=subset(MOstations,last_year>=2019 & (element=='PRCP'|element=='TMAX'|element=='TMIN'))
+
+View(ghcnd_search(uptonow[1,1]))               
