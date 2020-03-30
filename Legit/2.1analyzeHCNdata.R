73fc5dda5e9b297b2c531a5d32e3a667670e1b33
jdpeiffer6 <jdp6n8@mail.missouri.edu>
2020-03-30 17:18:32 -0500

    Added some stuff

---
+++ Legit/2.1analyzeHCNdata.R
@@ -0,0 +1,29 @@
+setwd("C:\\Users\\JD\\Documents\\StateParks\\Legit")
+load("allprcp.Rdata")
+#View(allprcp)
+
+allprcp=subset(allprcp,!is.na(allprcp$date))
+
+# Count NAs ---------------------------------------------------------------
+addNA=function(x){
+  return(sum(is.na(x)))
+}
+missingDate=allprcp[,6:dim(allprcp)[2]]*0
+
+for(i in 1:dim(allprcp)[1]){
+  missingDate[i,]=apply(allprcp[i:dim(allprcp)[1],6:dim(allprcp)[2]],2,addNA)
+  #print(i/18000)
+}
+save(missingDate,file="missingDate.Rdata")
+
+
+load("missingDate.Rdata")
+sumgt=function(x){
+  return(sum(x<100))
+}
+missings=apply(missingDate,1,sumgt)
+
+plot(1:18339,missings,xlab='Days cut since 1970',
+     ylab='Sites with less than 100 missing days',
+     main='Cutoff date vs Missing days'
+)
