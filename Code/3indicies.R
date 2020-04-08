5d6a7bdbb7883e262a4648c819f4f869080f92c9
jdpeiffer6 <jdp6n8@mail.missouri.edu>
2020-04-08 10:33:40 -0500

    did some things

--- Code/3indicies.R
+++ Code/3indicies.R
@@ -1,5 +1,7 @@
 setwd("C:\\Users\\JD\\Documents\\StateParks\\Data")
 library(climdex.pcic)
+library(ggplot2)
+library(hydroTSM)
 load("allprcp.Rdata")
 
 
@@ -7,14 +9,20 @@ comp.cwd=function(data){
   dat=climdexInput.raw(prec=data,prec.dates=as.PCICt(as.character(allprcp$date),cal="365_day"),base.range=c(1970,1971))
   return(climdex.cwd(dat))
 }
+addNA=function(x){
+  #this function adds up all of the NA values in a vector. in this case 
+  #we will use it on every column (station) of the dataset!
+  return(sum(is.na(x)))
+}
+
 cwd=apply(allprcp[,6:dim(allprcp)[2]],2,FUN=comp.cwd)
 save(cwd,file="cwd.Rdata")
 
-
-
-#start.date <- as.Date("2017-01-01")
-#end.date <- as.Date("2020-03-17")
-#dates = dip(start.date, end.date)
-#data=rnorm(length(dates),10,1)
-#test=climdexInput.raw(prec=data,prec.dates = as.PCICt(as.character(dates),cal="365_day"),base.range=c(2017,2018))
-#climdex.cwd(test)
+nas=apply(cwd,2,addNA)
+sum(nas<=10)
+cwdL=cwd[,nas<=10]
+cwdL=apply(cwdL,1,sum,na.rm=T)
+d=data.frame(yr=yip("1970-01-01","2019-01-01"),wet.days=cwdL[1:50])
+ggplot(data=d,aes(x=yr,y=wet.days,group=1))+geom_point(col="blue",size=2)+scale_x_date(date_labels = "%Y")+
+  labs(x="Year", y="Total Consecutive Wet Days\nacross 33 Missouri Stations",title="Consecutive Wet Days vs Time")+
+  theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"))
